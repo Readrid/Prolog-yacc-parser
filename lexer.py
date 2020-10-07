069c9aa51733da437daf4c7428a7deef4770839e
Readrid <890readrid@gmail.com>
2020-10-07 17:42:02 +0300

    Add simple version

--- lexer.py
+++ lexer.py
@@ -1,62 +1,31 @@
 import ply.lex as lex
 
+tokens = [
+    'ID',
+    'DOT',
+    'LPAREN',
+    'RPAREN',
+    'CORKSCREW',
+    'DISJ',
+    'CONJ'
+]
 
-class LexerLog(object):
-    def __init__(self, fileName, **kwargs):
-        self.lexer = lex.lex(module=self, **kwargs)
-        with open(fileName, 'r') as inputFile:
-            self.data = ''.join(line for line in inputFile.readlines())
-        self.lexer.input(self.data)
+t_ID = r'[a-zA-Z_][a-zA-Z_0-9]*'
+t_DOT = r'\.'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_CORKSCREW = r':-'
+t_DISJ = r';'
+t_CONJ = r','
 
-    tokens = [
-        'ID',
-        'DOT',
-        'LPAREN',
-        'RPAREN',
-        'CORKSCREW',
-        'DISJ',
-        'CONJ'
-    ]
+t_ignore = ' \t'
 
-    t_ID = r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t_DOT = r'\.'
-    t_LPAREN  = r'\('
-    t_RPAREN  = r'\)'
-    t_CORKSCREW = r':-'
-    t_DISJ = r';'
-    t_CONJ = r','
+def t_newline(t): 
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
-    t_ignore = ' \t'
+def t_error(t): 
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
 
-    def t_newline(self, t): 
-        r'\n+'
-        t.lexer.lineno += len(t.value)
-
-    def t_error(self, t): 
-        print("Illegal character '%s'" % t.value[0])
-        t.lexer.skip(1)
-
-    def getColumn(self):
-        if not self.lexer.token():
-            dop = len(self.tok.value)
-        else:
-            dop = 0
-        line_start = self.data.rfind('\n', 0, self.tok.lexpos) + 1
-        return (self.tok.lexpos - line_start) + dop
-
-    def getNextTok(self):
-        newTok = self.lexer.token()
-        if not newTok:
-            return False
-        self.tok = newTok
-        return self.tok.type
-
-    def getLine(self):
-        return self.tok.lineno
-
-    def test(self):
-        while True:
-            self.tok = self.lexer.token()
-            if not self.tok:
-                break
-            print(self.tok, sep=' ')
+lexer = lex.lex()
--- parser.py
+++ parser.py
@@ -1,62 +1,53 @@
 import ply.yacc as yacc 
 
-from lexer import LexerLog
+from lexer import tokens
 
-class Parser(object):
-    def __init__(self, data):
-        self.lex = LexerLog(data)
-        self.parser = yacc.yacc(module=self)
 
-    tokens = LexerLog.tokens
+def p_relation(p):
+    '''relation : atomexpr CORKSCREW disjunction DOT
+                | atomexpr DOT'''
+    pass
 
-    def p_relation(self, p):
-        '''relation : atomexpr CORKSCREW disjunction DOT
-                    | atomexpr DOT'''
+def p_atombody(p):
+    '''atombody : ID atom'''
+    pass
 
-        pass
+def p_atomexpr(p):
+    '''atomexpr : ID
+                | atombody'''
+    pass
 
-    def p_atombody(self, p):
-        '''atombody : ID atom'''
-        pass
+def p_atom(p):
+    '''atom : atomexpr 
+            | LPAREN atombody RPAREN
+            | LPAREN atombody RPAREN atom'''
+    pass
 
-    def p_atomexpr(self, p):
-        '''atomexpr : ID
-                    | atombody'''
-        pass
+def p_disjunction(p):
+    '''disjunction : conjunction 
+                   | conjunction DISJ disjunction'''
+    pass
 
-    def p_atom(self, p):
-        '''atom : atomexpr 
-                | LPAREN atombody RPAREN
-                | LPAREN atombody RPAREN atom'''
-        pass
+def p_conjunction(p):
+    '''conjunction : identifier
+                   | identifier CONJ conjunction'''
+    pass
 
-    def p_disjunction(self, p):
-        '''disjunction : conjunction 
-                       | conjunction DISJ disjunction'''
-        pass
+def p_identifier(p):
+    '''identifier : ID
+                  | LPAREN disjunction RPAREN'''
+    pass
 
-    def p_conjunction(self, p):
-        '''conjunction : identifier
-                      | identifier CONJ conjunction'''
-        pass
+def p_error(p):
+    print("Syntax error {0}, {1}".format(p.lineno, p.lexpos))
 
-    def p_identifier(self, p):
-        '''identifier : ID
-                      | LPAREN disjunction RPAREN'''
-        pass
+parser = yacc.yacc()
 
-    def p_error(self, p):
-        print("Syntax error {0}, {1}".format(p.lineno, p.lexpos))
-
-    def parse(self):
-        '''while True:
-            try:
-                s = input('calc > ')
-            except EOFError:
-                break
-            if not s: continue
-            result = self.parser.parse(s)
-            print(result)'''
-        print(self.lex.data)
-        result = self.parser.parse(self.lex.data)
-        print(result)
+while True:
+    try:
+        s = input('calc > ')
+    except EOFError:
+        break
+    if not s: continue
+    result = parser.parse(s)
+    print(result)
